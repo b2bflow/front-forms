8ec9e8488080cdd1998e065b692244b816b34b9a
Carlos Erick <carlos@empresa.com>
2026-02-11 10:27:40 -0300

    altereando a exibição do header no mobile

--- src/components/LeadForm/Header.tsx
+++ src/components/LeadForm/Header.tsx
@@ -10,14 +10,21 @@ export const Header = () => {
       className="backdrop-blur-sm border border-border rounded-2xl p-4 mb-8 flex items-center justify-between shadow-xl "
     >
       <div className="flex items-center gap-3">
-        <img src={logo} alt="B2B Flow" className="h-10 w-10 rounded-lg object-cover" />
+        <img src={logo} alt="B2BFlow" className="h-10 w-10 rounded-lg object-cover" />
         <span className="font-bold text-xl text-black">
           B2b<span className="text-primary">Flow</span>
         </span>
       </div>
-      <p className="text-sm text-black">
+      <p className="
+        text-black 
+        text-center sm:text-right 
+        max-w-[220px] sm:max-w-none 
+        leading-tight sm:leading-normal
+      ">
         A agência das empresas que crescem com{" "}
-        <span className="text-primary font-semibold">Inteligência Artificial</span>
+        <span className="text-primary font-semibold block sm:inline">
+          Inteligência Artificial
+        </span>
       </p>
     </motion.header>
   );
